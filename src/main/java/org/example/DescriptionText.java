74f0794a489a6e13024348e248456d39830606ed
KlaraLundquist <klaralundqvist@awacademy.training>
2022-08-23 17:30:16 +0200

    Initial commit

--- src/main/java/org/example/DescriptionText.java
+++ src/main/java/org/example/DescriptionText.java
@@ -8,10 +8,12 @@ import java.io.IOException;
 public class DescriptionText {
     private String start;
     private String gameOver;
+    private String level;
 
-    public DescriptionText(String start, String gameOver) {
+    public DescriptionText(String start, String gameOver, String level) {
         this.start = start;
         this.gameOver = gameOver;
+        this.level = level;
     }
     public String getStart() {
         return start;
@@ -19,15 +21,29 @@ public class DescriptionText {
     public String getGameOver() {
         return gameOver;
     }
+    public String getLevel() {
+        return level;
+    }
 
-    public void description(String string) throws IOException {
-        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
-        Terminal terminal = terminalFactory.createTerminal();
-
+    public void gameOverText(String string, Terminal terminal) throws IOException {
         for (int i = 0; i < string.length(); i++) {
             terminal.setCursorPosition(30+i, 15);
             terminal.putCharacter(string.charAt(i));
         }
         terminal.flush();
+    }
+    public void startText(String string, Terminal terminal) throws IOException {
+        for (int i = 0; i < string.length(); i++) {
+            terminal.setCursorPosition(0+i, 0);
+            terminal.putCharacter(string.charAt(i));
+        }
+        terminal.flush();
+    }
+    public void displayLevel (String level, Terminal terminal) throws IOException {
+        for (int i = 0; i < level.length(); i++) {
+            terminal.setCursorPosition(12, 0);
+            terminal.putCharacter(level.charAt(i));
+        }
+
     }
 }
