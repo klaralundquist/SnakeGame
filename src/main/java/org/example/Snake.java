750018d8f08342e21dd93dfaff9e01c4674117c4
KlaraLundquist <klaralundqvist@awacademy.training>
2022-08-24 10:55:38 +0200

    Added arraylist instead of array to method

--- src/main/java/org/example/Snake.java
+++ src/main/java/org/example/Snake.java
@@ -1,16 +1,16 @@
 package org.example;
-
 import com.googlecode.lanterna.terminal.Terminal;
-
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Snake {
     int size = 3;
 
-    public Position[] getBody(Terminal terminal) throws IOException {
-        Position[] snake = new Position[size];
-        for (int i = 0; i < snake.length; i++) {
-            snake[i] = new Position(10, 10+i);
+    public List<Position> getBody(Terminal terminal) throws IOException {
+        List<Position> snake = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            snake.add(new Position(10, 10+i));
         }
         for (Position p : snake) {
             terminal.setCursorPosition(p.x, p.y);
