8916f4289b2222106cdfebff2bc29b6a9bdb20b5
SebastianBarufke <sebastian.barufke@awacademy.training>
2022-08-25 12:03:50 +0200

    Ny poängräknare tillagd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,9 @@ import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -25,6 +27,7 @@ public class Main {
         DescriptionText start = new DescriptionText("SNAKE GAME", "GAME OVER");
         start.startText(start.start, terminal); // created text
 
+
         KeyStroke latestKeyStroke = null;
 
         boolean continueReadingInput = true;
@@ -34,13 +37,13 @@ public class Main {
             KeyStroke keyStroke = null;
             do {
                 index++;
-                if (index % 100 == 0) {
+                if (index % 60 == 0) {
                     if (latestKeyStroke != null) {
                         handleSnake(snake, latestKeyStroke, terminal, start, fruit, monster);
                     }
                 }
 
-                Thread.sleep(5);
+                Thread.sleep(3);
                 keyStroke = terminal.pollInput();
 
 
@@ -63,13 +66,51 @@ public class Main {
             case ArrowLeft -> snake.get(0).x -= 1;
         }
 
+        List<Position> toRemove = new ArrayList<>(); //array för att ta bort frukt som äts
+
         for (Position p : fruit) {
             if (p.x == head.x && p.y == head.y) {
                 terminal.setCursorPosition(tail.x, tail.y);
                 terminal.putCharacter('X');
                 snake.add(tail);
+
+                toRemove.add(p);
             }
         }
+
+        fruit.removeAll(toRemove); //tar bort frukt
+
+        //skapar ny frukt när snake äter en på random ställe:
+
+        if (fruit.size() < 4) {
+            Random r = new Random();
+            fruit.add(new Position(r.nextInt(50),r.nextInt(25)));
+            for (Position p : fruit) {
+                terminal.setCursorPosition(p.x, p.y);
+                terminal.putCharacter('F');
+            }
+        }
+        String size = String.valueOf(snake.size()-3);
+        for (int i = 0; i < size.length(); i++) {
+            terminal.setCursorPosition(63 + i, 0);
+            terminal.putCharacter('P');
+            terminal.setCursorPosition(64 + i, 0);
+            terminal.putCharacter('O');
+            terminal.setCursorPosition(65 + i, 0);
+            terminal.putCharacter('I');
+            terminal.setCursorPosition(66 + i, 0);
+            terminal.putCharacter('N');
+            terminal.setCursorPosition(67 + i, 0);
+            terminal.putCharacter('T');
+            terminal.setCursorPosition(68 + i, 0);
+            terminal.putCharacter('S');
+            terminal.setCursorPosition(69 + i, 0);
+            terminal.putCharacter(':');
+            terminal.setCursorPosition(70 + i, 0);
+
+            terminal.putCharacter(size.charAt(i));
+        }
+
         for (Position monsters : monster) {
             terminal.setCursorPosition(monsters.x, monsters.y);
             terminal.putCharacter(' ');
@@ -100,7 +141,11 @@ public class Main {
         terminal.setCursorPosition(head.x, head.y);
         terminal.putCharacter('X');
 
+
         terminal.flush();
+
+
+
 
         boolean crashIntoSnake = false;
 
@@ -117,6 +162,8 @@ public class Main {
             keyStroke.wait();
             terminal.flush();
         }
+
+
 
 
     }}
