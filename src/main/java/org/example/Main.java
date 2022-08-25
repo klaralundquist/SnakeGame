b20f8b9c7ecb51478d54c7891f0168d1cc55e54c
SebastianBarufke <sebastian.barufke@awacademy.training>
2022-08-25 10:36:43 +0200

    Lagt till Monster och att de följer efter player.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,20 +5,22 @@ import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
 import java.util.List;
-import java.util.Random;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
         Terminal terminal = terminalFactory.createTerminal();
-        terminal.setCursorVisible(true);
+        terminal.setCursorVisible(false);
 
         Snake s = new Snake();
         List<Position> snake = s.getBody(terminal); //get snake printed
 
-        Position food = new Position(25, 25); // added food
-        terminal.setCursorPosition(food.x, food.y);
-        terminal.putCharacter('O');
+        Fruit f = new Fruit();
+        List<Position> fruit = f.getFruits(terminal); // added fruits
+
+        Monster m = new Monster();
+        List<Position> monster = m.getMonster(terminal); // added monsters
+
 
         DescriptionText start = new DescriptionText("SNAKE GAME", "GAME OVER");
         start.startText(start.start, terminal); // created text
@@ -34,11 +36,11 @@ public class Main {
                 index++;
                 if (index % 100 == 0) {
                     if (latestKeyStroke != null) {
-                        handleSnake(snake, latestKeyStroke, terminal);
+                        handleSnake(snake, latestKeyStroke, terminal, start, fruit, monster);
                     }
                 }
 
-                Thread.sleep(3);
+                Thread.sleep(5);
                 keyStroke = terminal.pollInput();
 
 
@@ -48,56 +50,73 @@ public class Main {
 
         }
     }
-    private static void handleSnake(List<Position> snake, KeyStroke keyStroke, Terminal terminal) throws Exception {
+
+    private static void handleSnake(List<Position> snake, KeyStroke keyStroke, Terminal terminal, DescriptionText start, List <Position> fruit,List<Position> monster ) throws Exception {
         Position head = new Position(snake.get(0).x, snake.get(0).y);
-        Position tail = new Position(snake.get(snake.size()-1).x, snake.get(snake.size()-1).y);
+        Position tail = new Position(snake.get(snake.size() - 1).x, snake.get(snake.size() - 1).y);
         snake.add(0, head);
 
         switch (keyStroke.getKeyType()) {
-            case ArrowDown:
-                snake.get(0).y += 1;
-                break;
-            case ArrowUp:
-                snake.get(0).y -= 1;
-                break;
-            case ArrowRight:
-                snake.get(0).x += 1;
-                break;
-            case ArrowLeft:
-                snake.get(0).x -= 1;
-                break;
-        }
-        //Random
-
-        Random r = new Random();
-        Position fruitPosition = new Position(r.nextInt(80), r.nextInt(24));
-        terminal.setCursorPosition(fruitPosition.x,fruitPosition.y);
-        terminal.putCharacter('O');
-//Lägga till arrey list, loopar mot alla frukter
-
-        if(head.x == fruitPosition.x && head.y == fruitPosition.y){
-            terminal.setCursorPosition(tail.x, tail.y);
-            terminal.putCharacter('X');
-            snake.add(snake.get(snake.size()-1));
+            case ArrowDown -> snake.get(0).y += 1;
+            case ArrowUp -> snake.get(0).y -= 1;
+            case ArrowRight -> snake.get(0).x += 1;
+            case ArrowLeft -> snake.get(0).x -= 1;
         }
 
-
-        else {
-
-            //Draw snake:
-
-            terminal.setCursorPosition(tail.x, tail.y);
+        for (Position p : fruit) {
+            if (p.x == head.x && p.y == head.y) {
+                terminal.setCursorPosition(tail.x, tail.y);
+                terminal.putCharacter('X');
+                snake.add(tail);
+            }
+        }
+        for (Position monsters : monster) {
+            terminal.setCursorPosition(monsters.x, monsters.y);
             terminal.putCharacter(' ');
-            snake.remove(snake.get(snake.size() - 1));
+
+            if (head.x > monsters.x) {
+                monsters.x++;
+            }
+            else if (head.x < monsters.x) {
+                monsters.x--;
+            }
+            if (head.y > monsters.y) {
+                monsters.y++;
+            }
+            else if (head.y < monsters.y) {
+                monsters.y--;
+            }
+
+            terminal.setCursorPosition(monsters.x, monsters.y);
+            terminal.putCharacter('Ʌ');
         }
+        terminal.flush();
+
+
+        terminal.setCursorPosition(tail.x, tail.y);
+        terminal.putCharacter(' ');
+        snake.remove(snake.get(snake.size() - 1));
 
         terminal.setCursorPosition(head.x, head.y);
         terminal.putCharacter('X');
 
         terminal.flush();
 
+        boolean crashIntoSnake = false;
+
+        for (int i = 0; i < snake.size(); i++) {
+            if (i > 0) {
+                if (snake.get(0).x == snake.get(i).x && snake.get(0).y == snake.get(i).y) {
+                    crashIntoSnake = true;
+                }
+            }
+        }
+        if (crashIntoSnake) {
+            terminal.clearScreen();
+            start.gameOverText(start.getGameOver(), terminal);
+            keyStroke.wait();
+            terminal.flush();
+        }
 
 
-
-    }
-}
+    }}
